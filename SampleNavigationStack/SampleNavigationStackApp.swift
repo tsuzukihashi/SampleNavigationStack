1867e5612b526dcc90ad24ae5a2911dcef1f7b43
rtsuzuki <rtsuzuki@yahoo-corp.jp>
2022-12-07 19:28:41 +0900

    Initial Commit

---
+++ SampleNavigationStack/SampleNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleNavigationStackApp.swift
+//  SampleNavigationStack
+//
+//  Created by rtsuzuki on 2022/12/07.
+//
+
+import SwiftUI
+
+@main
+struct SampleNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
